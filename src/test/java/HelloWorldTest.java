e470e08fd99c0e29cc319aee11bdeaea568d3257
iykozlov2 <iykozlov2@mts.ru>
2022-12-11 19:30:38 +0300

    l1ex3

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,8 +3,12 @@ import org.junit.jupiter.api.Test;
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld(){
+    public void testHelloWorld() {
         System.out.println("Hello, world!");
     }
 
+    @Test
+    public void lesson1Ex3() {
+        System.out.println("Hello from username");
+    }
 }

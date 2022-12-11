e85851e5c9677abb6f994257e1f7f37a380c592b
iykozlov2 <iykozlov2@mts.ru>
2022-12-11 20:24:14 +0300

    l1ex4

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
 
@@ -10,5 +12,13 @@ public class HelloWorldTest {
     @Test
     public void lesson1Ex3() {
         System.out.println("Hello from username");
+    }
+
+    @Test
+    public void lesson1Ex4() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.getBody().print();
     }
 }

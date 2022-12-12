5e2138d7f817914b9e2abc291ed0cc34776257f5
iykozlov2 <iykozlov2@mts.ru>
2022-12-12 22:48:52 +0300

    Class renamed test lesson2Ex5 added

---
+++ src/test/java/HomeWorkTest.java
@@ -0,0 +1,43 @@
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+
+public class HomeWorkTest {
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello, world!");
+    }
+
+    @Test
+    public void lesson1Ex3() {
+        System.out.println("Hello from username");
+    }
+
+    @Test
+    public void lesson1Ex4() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.getBody().print();
+    }
+
+    @Test
+    public void lesson2Ex5() {
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        response.prettyPrint();
+
+        String secondMessage = response.get("messages[1].message");
+        if (secondMessage == null) {
+            System.out.println("There is no second message");
+        } else {
+            System.out.println("\n Second message is: " +  secondMessage);
+
+        }
+    }
+}
